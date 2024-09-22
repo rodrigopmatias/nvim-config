d216c106f41d9464fde421fa84fc5a27c3fe3648
Rodrigo Matias <rodrigopmatias@gmail.com>
2024-09-22 00:52:04 -0300

    fix: pequenas adequações e atualização pacotes

--- lua/plugins/overridden.lua
+++ lua/plugins/overridden.lua
@@ -1,5 +1,13 @@
 return {
   {
+    "catppuccin/nvim",
+    opts = function(_, opts) opts.no_italic = true end,
+  },
+  {
+    "linux-cultist/venv-selector.nvim",
+    opts = function(_, opts) opts.name = { ".venv", "venv" } end,
+  },
+  {
     "nvim-neotest/neotest-python",
     opts = function(_, opts)
       opts.dap = { justMyCode = false }
